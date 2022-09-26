147767410a517ac658827028cb9b5f6c6ec081bc
Gopesh03 <113349591+Gopesh03@users.noreply.github.com>
2022-09-26 19:02:50 +0530

    Update crash_check.cpp

--- ArduCopter/crash_check.cpp
+++ ArduCopter/crash_check.cpp
@@ -1,7 +1,7 @@
 #include "Copter.h"
-
+//This code is observed by GOPESH KRISHNA YADAV. There is error while editing due to cygwin.
 // Code to detect a crash main ArduCopter code
-#define CRASH_CHECK_TRIGGER_SEC         3       // 3 seconds inverted indicates a crash. Changed to 3 second from 2 second by GOPESH KRISHNA YADAV
+#define CRASH_CHECK_TRIGGER_SEC         2       // 2 seconds inverted indicates a crash.
 #define CRASH_CHECK_ANGLE_DEVIATION_DEG 30.0f   // 30 degrees beyond target angle is signal we are out of control.
 #define CRASH_CHECK_ANGLE_MIN_DEG       15.0f   // vehicle must be leaning at least 10deg to trigger crash check.
 #define CRASH_CHECK_SPEED_MAX           10.0f   // vehicle must be moving at less than 15m/s to trigger crash check.
